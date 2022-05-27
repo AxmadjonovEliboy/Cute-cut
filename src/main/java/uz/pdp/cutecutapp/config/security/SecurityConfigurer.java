ba6a08ff9e645caddbc0eb397810ae8b95a09339
Shoxruh0912 <bekpulatovshoxruh9@gmail.com>
2022-05-28 02:55:53 +0500

    method level security added

--- src/main/java/uz/pdp/cutecutapp/config/security/SecurityConfigurer.java
+++ src/main/java/uz/pdp/cutecutapp/config/security/SecurityConfigurer.java
@@ -3,6 +3,7 @@ package uz.pdp.cutecutapp.config.security;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -17,6 +18,7 @@ import uz.pdp.cutecutapp.utils.JwtUtils;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
+@EnableGlobalMethodSecurity
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 
     public final static String[] WHITE_LIST = {
