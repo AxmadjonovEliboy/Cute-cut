0b4ad75b95293ea29741ce43ff6eba2805c1a4e2
Shoxruh0912 <bekpulatovshoxruh9@gmail.com>
2022-07-13 15:11:14 +0500

    file storage fixed

--- src/main/java/uz/pdp/cutecutapp/controller/file/AttachmentController.java
+++ src/main/java/uz/pdp/cutecutapp/controller/file/AttachmentController.java
@@ -1,187 +1,34 @@
 package uz.pdp.cutecutapp.controller.file;
 
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.util.FileCopyUtils;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import uz.pdp.cutecutapp.entity.file.Attachment;
-import uz.pdp.cutecutapp.repository.file.AttachmentRepository;
+import uz.pdp.cutecutapp.dto.responce.DataDto;
+import uz.pdp.cutecutapp.services.file.FileStorageService;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Optional;
-import java.util.UUID;
 
 
 @RestController
 @RequestMapping("/file")
 public class AttachmentController {
 
-    private static final String uploadingDirectoryProfile = "uploads";
+    private final FileStorageService fileStorageService;
 
-    private final AttachmentRepository attachmentRepository;
-
-    public AttachmentController(AttachmentRepository attachmentRepository) {
-        this.attachmentRepository = attachmentRepository;
-        try {
-            Files.createDirectories(Paths.get(uploadingDirectoryProfile));
-        } catch (Exception ex) {
-            throw new RuntimeException("Could not create the directory where the uploaded files will be stored.", ex);
-        }
+    public AttachmentController(FileStorageService fileStorageService) {
+        this.fileStorageService = fileStorageService;
     }
 
     @PostMapping(value = "/uploadSystem", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public String system_upload(@RequestParam("file") MultipartFile file) throws IOException {
-
-        long size = file.getSize();
-        String originalFilename = file.getOriginalFilename();
-        String contentType = file.getContentType();
-
-        Attachment attachment = new Attachment();
-        attachment.setSize(size);
-        attachment.setFileOriginalName(originalFilename);
-        attachment.setContentType(contentType);
-        String[] split = originalFilename.split("\\.");
-        String name = UUID.randomUUID() + "." + split[split.length - 1];
-        attachment.setName(name); // fayl nomi saqlanadi
-
-        Attachment save = attachmentRepository.save(attachment);
-
-        // serverga yuklash
-        Path path = Paths.get(uploadingDirectoryProfile + "\\" + name);
-        Files.copy(file.getInputStream(), path);
-
-        return "saqlandi id : " + save.getId();
-
+    public ResponseEntity<DataDto<Long>> system_upload(@RequestParam("file") MultipartFile file) {
+        return new ResponseEntity<>(fileStorageService.storeFile(file), HttpStatus.OK);
     }
 
     @GetMapping("/getFileFromSystem/{id}")
-    public void getFilesFromSystem(@PathVariable Long id, HttpServletResponse response) throws IOException {
-        Optional<Attachment> byId = attachmentRepository.findById(id);
-        if (byId.isPresent()) {
-            Attachment attachment = byId.get();
-            // attachmentga nom  o'rnatish yani uni kop'chirib olsa bo'ladimi yo'qmi
-            // faqat ko'rish uchunmi yoki :
-
-            // fayl nomi
-            response.setHeader("Content-Disposition",
-                    "attachment; filename = \""
-                            + attachment.getFileOriginalName() + "\"");
-
-            //type:
-            response.setContentType(attachment.getContentType());// nima bo'lasa shuni ayatadi
-            // content :
-
-            FileInputStream fileInputStream = new FileInputStream(uploadingDirectoryProfile + "\\" + attachment.getName());
-            FileCopyUtils.copy(fileInputStream, response.getOutputStream());
-
-        } else
-            System.out.println("not found");
+    public void getFilesFromSystem(@PathVariable Long id, HttpServletResponse response) {
+        fileStorageService.loadFileAsResource(id, response);
     }
-    // uploading to the DB when only one file comes
-//    @PostMapping("/upload")
-//    public String upload(@NotNull MultipartHttpServletRequest request) throws IOException {
-//        Iterator<String> fileNames = request.getFileNames();
-//        MultipartFile file = request.getFile(fileNames.next());
-//
-//        // file information
-//        assert file != null; //if dek tekshirganda
-//        String originalFilename = file.getOriginalFilename();
-//        long size = file.getSize();
-//        String contentType = file.getContentType();
-//
-//        Attachment attachment = new Attachment();
-//        attachment.setFileOriginalName(originalFilename);
-//        attachment.setContentType(contentType);
-//        attachment.setSize(size);
-//
-//        Attachment save = attachmentRepository.save(attachment);
-//
-//        // file content (asosiy byte[] larda saqlaymiz)  info
-//        byte[] bytes = file.getBytes();
-//
-//        AttachmentContent attachmentContent = new AttachmentContent();
-//
-//        attachmentContent.setBytes(bytes);
-//        attachmentContent.setAttachment(save);
-//
-//        attachmentContentRepository.save(attachmentContent);
-//        return "File saqlandi  id : " + save.getId();
-//    }
-
-    // upload to the system :
-
-
-    // getting from DB
-//    @GetMapping("/download/{id}")
-//    public void getFile(@PathVariable Integer id,
-//                        HttpServletResponse response) throws IOException {
-//
-//        Optional<Attachment> byId = attachmentRepository.findById(id);
-//        if (byId.isPresent()) {
-//            Attachment attachment = byId.get();
-//
-//            Optional<AttachmentContent> byAttachmentId = attachmentContentRepository.findByAttachmentId(attachment.getId());
-//            if (byAttachmentId.isPresent()) {
-//                AttachmentContent attachmentContent = byAttachmentId.get();
-//
-//                // fayl nomi
-//                response.setHeader("Content-Disposition",
-//                        "attachment; filename = \""
-//                                + attachment.getFileOriginalName() + "\"");
-//
-//                //type:
-//                response.setContentType(attachment.getContentType());// nima bo'lasa shuni ayatadi
-//                // content :
-//                FileCopyUtils.copy(attachmentContent.getBytes(), response.getOutputStream());
-//
-//
-//            }
-//        } else {
-//            throw new FileNotFoundException("File not found");
-//        }
-//    }
-
-//    @PutMapping("/{id}")
-//    public HttpEntity<?> editAttachment(@PathVariable Integer id, @NotNull MultipartHttpServletRequest request) throws IOException {
-//
-//        Optional<Attachment> optionalAttachment = attachmentRepository.findById(id);
-//        if (optionalAttachment.isEmpty()) {
-//            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("This attachment not found");
-//        }
-//
-//        Iterator<String> fileNames = request.getFileNames();
-//        MultipartFile file = request.getFile(fileNames.next());
-//
-//        // file information
-//        assert file != null; //if dek tekshirganda
-//        String originalFilename = file.getOriginalFilename();
-//        long size = file.getSize();
-//        String contentType = file.getContentType();
-//
-//        Attachment attachment = new Attachment();
-//        attachment.setFileOriginalName(originalFilename);
-//        attachment.setContentType(contentType);
-//        attachment.setSize(size);
-//
-//        Attachment save = attachmentRepository.save(attachment);
-//
-//        // file content (asosiy byte[] larda saqlaymiz)  info
-//        byte[] bytes = file.getBytes();
-//
-//        AttachmentContent attachmentContent = new AttachmentContent();
-//
-//        attachmentContent.setBytes(bytes);
-//        attachmentContent.setAttachment(save);
-//
-//        attachmentContentRepository.save(attachmentContent);
-//        return ResponseEntity.ok("File saqlandi  id : " + save.getId());
-//    }
-
-
 }
--- src/main/java/uz/pdp/cutecutapp/services/file/FileStorageService.java
+++ src/main/java/uz/pdp/cutecutapp/services/file/FileStorageService.java
@@ -1,77 +1,87 @@
 package uz.pdp.cutecutapp.services.file;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.multipart.MultipartFile;
-import uz.pdp.cutecutapp.dto.file.UploadsDto;
 import uz.pdp.cutecutapp.dto.responce.AppErrorDto;
 import uz.pdp.cutecutapp.dto.responce.DataDto;
-import uz.pdp.cutecutapp.entity.file.Uploads;
+import uz.pdp.cutecutapp.entity.file.Attachment;
 import uz.pdp.cutecutapp.properties.FileStorageProperties;
-import uz.pdp.cutecutapp.repository.file.UploadsRepository;
+import uz.pdp.cutecutapp.repository.file.AttachmentRepository;
 import uz.pdp.cutecutapp.services.BaseService;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class FileStorageService implements BaseService {
 
-    private final Path fileStorageLocation;
-    private final UploadsRepository uploadsRepository;
+    private final AttachmentRepository attachmentRepository;
+    private final FileStorageProperties fileStorageProperties;
 
     @Autowired
-    public FileStorageService(FileStorageProperties fileStorageProperties, UploadsRepository uploadsRepository) {
-        this.fileStorageLocation = Paths.get(fileStorageProperties.getUploadDir())
-                .toAbsolutePath().normalize();
-        this.uploadsRepository = uploadsRepository;
-//        try {
-//            Files.createDirectories(this.fileStorageLocation);
-//        } catch (Exception ex) {
-//            throw new RuntimeException("Could not create the directory where the uploaded files will be stored.", ex);
-//        }
+    public FileStorageService(AttachmentRepository attachmentRepository, FileStorageProperties fileStorageProperties) {
+        this.attachmentRepository = attachmentRepository;
+        this.fileStorageProperties = fileStorageProperties;
+        try {
+            Files.createDirectories(Paths.get(fileStorageProperties.getUploadDir()));
+        } catch (Exception ex) {
+            throw new RuntimeException("Could not create the directory where the uploaded files will be stored.", ex);
+        }
+
     }
 
-    public DataDto<Uploads> storeFile(MultipartFile file) {
-        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+    public DataDto<Long> storeFile(MultipartFile file) {
         try {
-            Path path = this.fileStorageLocation.resolve(fileName);
-            Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
-
+            long size = file.getSize();
             String originalFilename = file.getOriginalFilename();
-            String extension = StringUtils.getFilenameExtension(file.getOriginalFilename());
-            String generatedName = String.format("%s.%s", System.currentTimeMillis(), extension);
-            Uploads uploads = new Uploads(originalFilename, generatedName, file.getContentType(), path.toString(), file.getSize());
-            uploadsRepository.save(uploads);
-            return new DataDto<>(uploads);
-        } catch (IOException ex) {
-            return new DataDto<>(new AppErrorDto(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Could not store file " + fileName + ". Please try again!"));
+            String contentType = file.getContentType();
+
+            Attachment attachment = new Attachment();
+            attachment.setSize(size);
+            attachment.setFileOriginalName(originalFilename);
+            attachment.setContentType(contentType);
+            String[] split = originalFilename.split("\\.");
+            String name = UUID.randomUUID() + "." + split[split.length - 1];
+            attachment.setName(name);
+
+            Attachment save = attachmentRepository.save(attachment);
+
+            Path path = Paths.get(fileStorageProperties.getUploadDir()
+                    + fileStorageProperties.getDelimiter() + name);
+            Files.copy(file.getInputStream(), path);
+            return new DataDto<>(save.getId(), HttpStatus.CREATED.value());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new DataDto<>(new AppErrorDto(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage()));
         }
     }
 
-    public DataDto<Resource> loadFileAsResource(String fileName) {
-        try {
-            Optional<Uploads> byGeneratedName = uploadsRepository.findByGeneratedName(fileName);
-            Uploads uploads = byGeneratedName.get();
-            Path filePath = this.fileStorageLocation.resolve(uploads.getOriginalName()).normalize();
-            Resource resource = new UrlResource(filePath.toUri());
+    public void loadFileAsResource(Long id, HttpServletResponse response) {
 
-            if (resource.exists()) {
-                return new DataDto<>(resource);
-            } else {
-                return new DataDto<>(new AppErrorDto(HttpStatus.NOT_FOUND.value(), "File not found " + fileName));
+        Optional<Attachment> byId = attachmentRepository.findById(id);
+        if (byId.isPresent()) {
+            Attachment attachment = byId.get();
+            response.setHeader("Content-Disposition",
+                    "attachment; filename = \""
+                            + attachment.getFileOriginalName() + "\"");
+            response.setContentType(attachment.getContentType());
+            try {
+                FileInputStream fileInputStream = new FileInputStream(fileStorageProperties.getUploadDir()
+                        + fileStorageProperties.getDelimiter() + attachment.getName());
+                FileCopyUtils.copy(fileInputStream, response.getOutputStream());
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (MalformedURLException ex) {
-            return new DataDto<>(new AppErrorDto(HttpStatus.NOT_FOUND.value(), "File not found " + fileName));
-        }
+        } else
+            System.out.println("Content Not found with id : " + id);
     }
 }
